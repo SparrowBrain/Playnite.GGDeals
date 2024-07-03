924845ffd8ec534e014c814f85e1d5d022007540
SparrowBrain <qwxmail@gmail.com>
2024-07-03 21:58:57 +0300

    Added authentication token to settings

--- GGDeals/Settings/GGDealsSettings.cs
+++ GGDeals/Settings/GGDealsSettings.cs
@@ -5,8 +5,10 @@ namespace GGDeals.Settings
 {
 	public class GGDealsSettings : ObservableObject, IVersionedSettings
 	{
-		private string _devCollectionImportEndpoint;
 		public const int CurrentVersion = 0;
+
+		private string _authenticationToken;
+		private string _devCollectionImportEndpoint;
 
 		public GGDealsSettings()
 		{
@@ -14,16 +16,22 @@ namespace GGDeals.Settings
 		}
 
 		public static GGDealsSettings Default =>
-			new GGDealsSettings
-			{
-				LibrariesToSkip = new List<Guid>()
-				{
+		new GGDealsSettings
+		{
+			LibrariesToSkip = new List<Guid>()
+		{
 					Guid.Parse("CB91DFC9-B977-43BF-8E70-55F46E410FAB"), // Steam
                     Guid.Parse("AEBE8B7C-6DC3-4A66-AF31-E7375C6B5E9E"), // GOG
-                }
-			};
+					}
+		};
 
 		public int Version { get; set; }
+
+		public string AuthenticationToken
+		{
+			get => _authenticationToken;
+			set => SetValue(ref _authenticationToken, value);
+		}
 
 		public List<Guid> LibrariesToSkip { get; set; }
 
