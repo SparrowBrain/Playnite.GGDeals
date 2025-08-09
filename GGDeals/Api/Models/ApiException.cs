aed1f188eac156a9ea48ffd71f6d8a4dbd0d516d
SparrowBrain <qwxmail@gmail.com>
2025-08-09 22:16:37 +0300

    API failures are now propagated to the user

--- GGDeals/Api/Models/ApiException.cs
+++ GGDeals/Api/Models/ApiException.cs
@@ -1,16 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GGDeals.Api.Models
 {
-    public class ApiException:Exception
-    {
-        public ApiException(string message)
-        {
-            
-        }
-    }
+	public class ApiException : Exception
+	{
+		public ApiException(string message) : base(message)
+		{
+		}
+	}
 }
